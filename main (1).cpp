141be25996672e17defec1db30748920f4bc35d5
Manish Bhagat <95159709+Manish-Bhagat@users.noreply.github.com>
2022-03-01 22:56:50 +0530

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,26 @@
+using namespace std;
+#include<iostream>
+int main()
+{
+    int a=40;
+
+     switch(a)
+     {
+         case 10:
+         cout <<"case 10 is stored in a";
+         break;
+         
+         case 20:
+         cout<<"case 20 is stored in a";
+         break;
+         
+         case 40:
+         cout<<"case 40 is stored in a";
+         break;
+         
+         default:
+         cout<<"no case is matching";
+         break;
+     }
+    return 0;
+}
